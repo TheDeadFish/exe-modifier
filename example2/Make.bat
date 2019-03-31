88654ef18f27c6082d9c0b06163685595c17204b
TheDeadFish <michaelstamper1@gmail.com>
2019-03-31 10:29:46 +0000

    1. fixed example2 build, needed CCFLAGS

--- example2/Make.bat
+++ example2/Make.bat
@@ -1,5 +1,5 @@
 call exmod.bat
 ..\bin\cc_data happiness_island.mod music.cpp musicData
-gcc *.cc %CFLAGS2% -c
+gcc *.cc %CCFLAGS2% -c
 exe_mod.exe calc.exe music_calc.exe calc.def *.o -lwinmm libxmp-coremod.a -lstdshit
 del *.o music.cpp
