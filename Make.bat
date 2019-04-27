bab6cb07ff1f65b19a5cbd32f09d314af9935dab
TheDeadFish <michaelstamper1@gmail.com>
2019-04-27 10:01:55 +0000

    1. make.bat: supressed echo for quieter build

--- Make.bat
+++ Make.bat
@@ -1,15 +1,14 @@
-setlocal
-call egcc.bat
-pushd build
-%CMAKE% -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%" ..\.
-mingw32-make
-popd
+@setlocal
+@call egcc.bat
+@pushd build
+@%CMAKE% -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%" ..\.
+@mingw32-make
+@popd
 
-if exist %PROGRAMS%\progs\exe_mod (
-  copy /Y bin\exe_mod.exe %PROGRAMS%\progs\exe_mod
-  copy /Y bin\df_link.exe %PROGRAMS%\progs\exe_mod
-  copy /Y bin\libmisc32.a %PROGRAMS%\progs\exe_mod
-  copy /Y bin\libmisc64.a %PROGRAMS%\progs\exe_mod
-  copy /Y bin\exe_mod.cfg %PROGRAMS%\progs\exe_mod
-  copy /Y bin\setup.vbs %PROGRAMS%\progs\exe_mod
+@if exist %PROGRAMS%\progs\exe_mod (
+@  copy /Y bin\exe_mod.exe %PROGRAMS%\progs\exe_mod
+@  copy /Y bin\libmisc32.a %PROGRAMS%\progs\exe_mod
+@  copy /Y bin\libmisc64.a %PROGRAMS%\progs\exe_mod
+@  copy /Y bin\exe_mod.cfg %PROGRAMS%\progs\exe_mod
+@  copy /Y bin\setup.vbs %PROGRAMS%\progs\exe_mod
 )
