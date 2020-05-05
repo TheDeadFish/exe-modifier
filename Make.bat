970c4c8d720ada485316937969d0557f116a31a8
TheDeadFish <michaelstamper1@gmail.com>
2020-05-05 12:40:36 +0000

    Make.bat: use new cmake_gcc.bat build script

--- Make.bat
+++ Make.bat
@@ -1,12 +1,4 @@
-@setlocal
-@call egcc.bat
-@pushd build
-@set CFLAGS=%CFLAGS2%
-@set CXXFLAGS=%CCFLAGS2%
-::@%CMAKE% -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%" ..\.
-@%CMAKE%  ..\.
-@mingw32-make
-@popd
+@call cmake_gcc
 
 @if exist %PROGRAMS%\progs\exe_mod (
 @  copy /Y bin\exe_mod.exe %PROGRAMS%\progs\exe_mod
